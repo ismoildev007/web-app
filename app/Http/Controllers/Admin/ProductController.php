279bf457c7122e3cab47c75ae9c48959bb5dd4de
Ismoil Dev <ismoil3436@gmail.com>
2025-06-20 11:31:19 +0500

    commit

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -34,15 +34,9 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name_uz' => 'required|string|max:255',
-            'name_ru' => 'required|string|max:255',
-            'name_en' => 'required|string|max:255',
-            'content_uz' => 'nullable|string',
-            'content_ru' => 'nullable|string',
-            'content_en' => 'nullable|string',
-            'description_uz' => 'nullable|string',
-            'description_ru' => 'nullable|string',
-            'description_en' => 'nullable|string',
+            'name' => 'required|string|max:255',
+            'content' => 'nullable|string',
+            'description' => 'nullable|string',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,webp,svg|max:4096',
         ]);
 
@@ -52,15 +46,9 @@ class ProductController extends Controller
         }
 
         Product::create([
-            'name_uz' => $request->name_uz,
-            'name_ru' => $request->name_ru,
-            'name_en' => $request->name_en,
-            'content_uz' => $request->content_uz,
-            'content_ru' => $request->content_ru,
-            'content_en' => $request->content_en,
-            'description_uz' => $request->description_uz,
-            'description_ru' => $request->description_ru,
-            'description_en' => $request->description_en,
+            'name' => $request->name,
+            'content' => $request->content,
+            'description' => $request->description,
             'image' => $image,
         ]);
 
@@ -78,15 +66,9 @@ class ProductController extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
-            'name_uz' => 'required|string|max:255',
-            'name_ru' => 'required|string|max:255',
-            'name_en' => 'required|string|max:255',
-            'content_uz' => 'nullable|string',
-            'content_ru' => 'nullable|string',
-            'content_en' => 'nullable|string',
-            'description_uz' => 'nullable|string',
-            'description_ru' => 'nullable|string',
-            'description_en' => 'nullable|string',
+            'name' => 'required|string|max:255',
+            'content' => 'nullable|string',
+            'description' => 'nullable|string',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,webp,svg|max:4096',
         ]);
 
@@ -101,15 +83,9 @@ class ProductController extends Controller
         }
 
         $product->update([
-            'name_uz' => $request->name_uz,
-            'name_ru' => $request->name_ru,
-            'name_en' => $request->name_en,
-            'content_uz' => $request->content_uz,
-            'content_ru' => $request->content_ru,
-            'content_en' => $request->content_en,
-            'description_uz' => $request->description_uz,
-            'description_ru' => $request->description_ru,
-            'description_en' => $request->description_en,
+            'name' => $request->name,
+            'content' => $request->content,
+            'description' => $request->description,
             'image' => $image,
         ]);
 
